f92346866bd430505e921a1a8695a0a012beb572
1715173329 <22235437+1715173329@users.noreply.github.com>
2018-01-27 01:48:27 +0800

    Add files via upload

--- checkinfo.sh
+++ checkinfo.sh
@@ -2,6 +2,15 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
+# Make sure only root can run our script
+function rootness(){
+    if [[ $EUID -ne 0 ]]; then
+       echo "Error:This script must be run as root,please run 'sudo su' first." 1>&2
+       exit 1
+    fi
+}
+
+
 function checkenv(){
     if [ ! -z "`cat /etc/issue | grep bian`" ];then
         OS='debian'
--- checkos.sh
+++ checkos.sh
@@ -2,6 +2,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
+# Make sure only root can run our script
 function rootness(){
     if [[ $EUID -ne 0 ]]; then
        echo "Error:This script must be run as root,please run 'sudo su' first." 1>&2
@@ -10,7 +11,7 @@ function rootness(){
 }
 
 
-function checkenv(){
+function checkos(){
     rootness
     if [ -f /etc/redhat-release ];then
         echo "Your OS type is CentOS."
@@ -23,4 +24,4 @@ function checkenv(){
         exit 1
     fi
 }
-    checkenv
+    checkos
