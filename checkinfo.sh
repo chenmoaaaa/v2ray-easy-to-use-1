099a2a92434bf6386f7aed0184f7586aff80e4e2
1715173329 <22235437+1715173329@users.noreply.github.com>
2018-01-27 01:35:17 +0800

    Add files via upload

--- checkinfo.sh
+++ checkinfo.sh
@@ -5,10 +5,11 @@ export PATH
 # Make sure only root can run our script
 function rootness(){
     if [[ $EUID -ne 0 ]]; then
-       echo "Error:This script must be run as root!" 1>&2
+       echo "Error:This script must be run as root,please run 'sudo su' first." 1>&2
        exit 1
     fi
 }
+
 
 function checkos(){
     if [ ! -z "`cat /etc/issue | grep bian`" ];then
