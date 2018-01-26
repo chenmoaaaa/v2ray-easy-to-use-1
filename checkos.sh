c1ba50eb83c8e1be23974d716897fdcfb60b35be
1715173329 <22235437+1715173329@users.noreply.github.com>
2018-01-27 03:19:43 +0800

    Add files via upload

--- checkos.sh
+++ checkos.sh
@@ -1,27 +1,8 @@
-#! /bin/bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
-
-# Make sure only root can run our script
-function rootness(){
-    if [[ $EUID -ne 0 ]]; then
-       echo "Error:This script must be run as root,please run 'sudo su' first." 1>&2
-       exit 1
-    fi
-}
-
-
-function checkos(){
-    rootness
-    if [ -f /etc/redhat-release ];then
-        echo "Your OS type is CentOS."
-    elif [ ! -z "`cat /etc/issue | grep bian`" ];then
-        echo "Your OS type is Debian."
+    if [ ! -z "`cat /etc/issue | grep bian`" ];then
+        OS='debian'
     elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
-        echo "Your OS type is Ubuntu."
+        OS='ubuntu'
     else
-        echo "Not support OS, Please change OS and retry!"
+        echo "Not support OS, Please reinstall OS and retry!"
         exit 1
     fi
-}
-    checkos
