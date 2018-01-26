d054e38a294c546c896db64810fdfdee64d0e4c4
1715173329 <22235437+1715173329@users.noreply.github.com>
2018-01-27 01:39:26 +0800

    Add files via upload

--- checkinfo.sh
+++ checkinfo.sh
@@ -2,15 +2,6 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-# Make sure only root can run our script
-function rootness(){
-    if [[ $EUID -ne 0 ]]; then
-       echo "Error:This script must be run as root!" 1>&2
-       exit 1
-    fi
-}
-
-
 function checkos(){
     if [ ! -z "`cat /etc/issue | grep bian`" ];then
         OS='debian'
