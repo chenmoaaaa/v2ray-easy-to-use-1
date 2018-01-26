603f03cdba7603c4ac74f88fcbb742ce812e274c
1715173329 <22235437+1715173329@users.noreply.github.com>
2018-01-27 01:21:57 +0800

    Add files via upload

---
+++ checkos.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+# Make sure only root can run our script
+function rootness(){
+    if [[ $EUID -ne 0 ]]; then
+       echo "Error:This script must be run as root!" 1>&2
+       exit 1
+    fi
+}
+
+function checkenv(){
+    if [ -f /etc/redhat-release ];then
+        echo "Your OS type is CentOS."
+    elif [ ! -z "`cat /etc/issue | grep bian`" ];then
+        echo "Your OS type is Debian."
+    elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
+        echo "Your OS type is Ubuntu."
+    else
+        echo "Not support OS, Please change OS and retry!"
+        exit 1
+    fi
+}
