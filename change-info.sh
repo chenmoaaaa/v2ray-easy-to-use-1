80625557a525eb36a2b13637418baa8e96964114
1715173329 <22235437+1715173329@users.noreply.github.com>
2018-01-27 18:31:39 +0800

    Add files via upload

--- change-info.sh
+++ change-info.sh
@@ -25,22 +25,24 @@ function checkos(){
 }
 
  
-#  Install v2ray
 function change_info(){
 	rootness
 	checkos
 	let PORT=$RANDOM+10000 && UUID=$(cat /proc/sys/kernel/random/uuid) && hostname=$(hostname) && sed -i "s/10000/${PORT}/g" "/etc/v2ray/config.json" && sed -i "s/3922f464-d02d-4124-82bf-ad350c19aacf/${UUID}/g" "/etc/v2ray/config.json" && sed -i "s/10000/${PORT}/g" "/usr/local/caddy/Caddyfile"
-	vim /usr/local/caddy/Caddyfile
 	service v2ray restart && service caddy restart
 	Address0=$(sed -n '1p' "/usr/local/caddy/Caddyfile") 
 	Address=${Address0#*"://"}
 	mkdir -p /root/v2ray-go/
 	cd /root/
-	echo  "这是您的连接信息：" 
-	echo "别名(Remarks)：${hostname}" 
-	echo "地址(Address)：${Address}" >> "./v2ray-go/v2info.txt" &&echo "端口(Port):443" >> "./v2ray-go/v2info.txt" &&echo "用户ID(ID):${UUID}" >> "./v2ray-go/v2info.txt" &&echo "额外ID(AlterID):64" >> "./v2ray-go/v2info.txt" &&echo "加密方式(Security)：none" >> "./v2ray-go/v2info.txt" &&	echo "伪装类型(Type）：none" >> "./v2ray-go/v2info.txt" &&echo "伪装域名/其他项：/liping" >> "./v2ray-go/v2info.txt" &&echo "底层传输安全(TLS)：tls" >> "./v2ray-go/v2info.txt"&&echo "V2ray配置位置(config.json)：/etc/v2ray/config.json" >> "./v2ray-go/v2info.txt"  
-	cat "./v2ray-go/v2info.txt" 
-	echo "本文件保存在：/root/v2ray-go/v2info.txt"
-
+	echo -e "这是您的连接信息：\n" 
+	echo -e "别名(Remarks)：${hostname}\n" 
+	echo -e "地址(Address)：${Address}\n"
+	echo -e "端口(Port):443\n"
+	echo -e "用户ID(ID):${UUID}\n"
+	echo -e "额外ID(AlterID):64\n"
+	echo -e "加密方式(Security)：none\n"
+	echo -e "伪装类型(Type）：none\n"
+	echo -e "伪装域名/其他项：/liping\n"
+	echo -e "底层传输安全(TLS)：tls\n"
 }
     change_info
