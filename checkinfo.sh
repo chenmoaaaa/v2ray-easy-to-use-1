8d04cd84c95be9f4c487783d14a30254ffe2df6f
1715173329 <22235437+1715173329@users.noreply.github.com>
2018-01-27 22:25:10 +0800

    Update checkinfo.sh

--- checkinfo.sh
+++ checkinfo.sh
@@ -24,24 +24,24 @@ function checkenv(){
 
 function change_configs(){
     rootness
-	checkenv
-	let PORT=$RANDOM+10000
-	UUID=$(cat /proc/sys/kernel/random/uuid)
-	hostname=$(hostname) && sed -i "s/10000/${PORT}/g" "/etc/v2ray/config.json"
-	sed -i "s/3922f464-d02d-4124-82bf-ad350c19aacf/${UUID}/g" "/etc/v2ray/config.json"
-	sed -i "s/10000/${PORT}/g" "/usr/local/caddy/Caddyfile"
-	vim /usr/local/caddy/Caddyfile
-	service v2ray restart && service caddy restart
-	Address=$(sed -n '1p' "/usr/local/caddy/Caddyfile") 
-	echo -e "这是您的连接信息：\n" 
-	echo -e "别名(Remarks)：${hostname}\n" 
-	echo -e "地址(Address)：${Address}\n"
-	echo -e "端口(Port):443\n"
-	echo -e "用户ID(ID):${UUID}\n"
-	echo -e "额外ID(AlterID):100\n"
-	echo -e "加密方式(Security)：none\n"
-	echo -e "伪装类型(Type）：none\n"
-	echo -e "伪装域名/其他项：/tmp/video\n"
-	echo -e "底层传输安全(TLS)：tls\n"
+    checkenv
+    let PORT=$RANDOM+10000
+    UUID=$(cat /proc/sys/kernel/random/uuid)
+    hostname=$(hostname) && sed -i "s/10000/${PORT}/g" "/etc/v2ray/config.json"
+    sed -i "s/3922f464-d02d-4124-82bf-ad350c19aacf/${UUID}/g" "/etc/v2ray/config.json"
+    sed -i "s/10000/${PORT}/g" "/usr/local/caddy/Caddyfile"
+    vim /usr/local/caddy/Caddyfile
+    service v2ray restart && service caddy restart
+    Address=$(sed -n '1p' "/usr/local/caddy/Caddyfile") 
+    echo -e "这是您的连接信息：\n" 
+    echo -e "别名(Remarks)：${hostname}\n" 
+    echo -e "地址(Address)：${Address}\n"
+    echo -e "端口(Port):443\n"
+    echo -e "用户ID(ID):${UUID}\n"
+    echo -e "额外ID(AlterID):100\n"
+    echo -e "加密方式(Security)：none\n"
+    echo -e "伪装类型(Type）：none\n"
+    echo -e "伪装域名/其他项：/tmp/video\n"
+    echo -e "底层传输安全(TLS)：tls\n"
 }
     change_configs
