1b865b0d1777cb950a1041c4de5f24a9e345a08f
1715173329 <22235437+1715173329@users.noreply.github.com>
2018-01-28 02:14:46 +0800

    Update change-info.sh

--- change-info.sh
+++ change-info.sh
@@ -28,6 +28,8 @@ function change_info(){
     rootness
     checkos
     let PORT=$RANDOM+10000
+    UUID=$(cat /proc/sys/kernel/random/uuid)
+    hostname=$(hostname)
     sed -i "s/10000/${PORT}/g" "/etc/v2ray/config.json"
     sed -i "s/3922f464-d02d-4124-82bf-ad350c19aacf/${UUID}/g" "/etc/v2ray/config.json"
     sed -i "s/10000/${PORT}/g" "/usr/local/caddy/Caddyfile"
@@ -38,8 +40,6 @@ function change_info(){
     rm -rf /tmp/caddyaddress.txt
     echo -e "您的域名为: ${Address}"
     sed -i "s#V2rayAddress#https://${Address}#g" "/usr/local/caddy/Caddyfile"
-    UUID=$(cat /proc/sys/kernel/random/uuid)
-    hostname=$(hostname)
     service v2ray restart && service caddy restart
     echo -e "\n这是您的连接信息：\n别名(Remarks)：${hostname}\n地址(Address)：${Address}\n端口(Port):443\n用户ID(ID):${UUID}\n额外ID(AlterID):100\n加密方式(Security)：none\n伪装类型(Type）：none\n伪装域名/其他项：/tmp/video\n底层传输安全(TLS)：tls\n"
 }
