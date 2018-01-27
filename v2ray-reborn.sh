60a201ebe8ed796158f50caec56f276c31d2ad04
1715173329 <22235437+1715173329@users.noreply.github.com>
2018-01-27 23:22:44 +0800

    Add files via upload

---
+++ v2ray-reborn.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+# Make sure only root can run our script
+function rootness(){
+    if [[ $EUID -ne 0 ]]; then
+       echo "Error:This script must be run as root,please run 'sudo su' first." 1>&2
+       exit 1
+    fi
+
+}
+ 
+ 
+function checkos(){
+    if [ -f /etc/redhat-release ];then
+        OS='centos'
+    elif [ ! -z "`cat /etc/issue | grep bian`" ];then
+        OS='debian'
+    elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
+        OS='ubuntu'
+    else
+        echo "Not support OS, Please change OS and retry!"
+        exit 1
+    fi
+}
+
+
+function checkenv(){
+    if [[ $OS = "centos" ]]; then
+        yum install wget unzip vim curl -y
+    else
+        apt-get -y update
+        apt-get -y install wget unzip vim curl
+    fi
+}
+
+
+function reborn_v2ray(){
+    checkos
+    rootness
+	checkenv
+	curl https://www.v2ray-install.ml/v2ray-remove.sh | bash
+	curl https://www.v2ray-install.ml/v2ray-go.sh | bash
+}
+    reborn_v2ray
