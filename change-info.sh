169bcd86d29ecd2a787482931ad4824f199c98f8
1715173329 <22235437+1715173329@users.noreply.github.com>
2018-01-28 00:10:20 +0800

    Update change-info.sh

--- change-info.sh
+++ change-info.sh
@@ -36,6 +36,6 @@ function change_info(){
     sed -i "s/10000/${PORT}/g" "/usr/local/caddy/Caddyfile"
     service v2ray restart && service caddy restart
     Address=$(sed -n '1p' "/usr/local/caddy/Caddyfile") 
-    echo -e "这是您的连接信息：\n别名(Remarks)：${hostname}\n地址(Address)：${Address}\n端口(Port):443\n用户ID(ID):${UUID}\n额外ID(AlterID):100\n加密方式(Security)：none\n伪装类型(Type）：none\n伪装域名/其他项：/tmp/video\n底层传输安全(TLS)：tls\n"
+    echo -e "\n这是您的连接信息：\n别名(Remarks)：${hostname}\n地址(Address)：${Address}\n端口(Port):443\n用户ID(ID):${UUID}\n额外ID(AlterID):100\n加密方式(Security)：none\n伪装类型(Type）：none\n伪装域名/其他项：/tmp/video\n底层传输安全(TLS)：tls\n"
 }
     change_info
