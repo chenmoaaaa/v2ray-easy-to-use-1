7f978d9eef416dd24926cb70e8180dc5ca231dc6
1715173329 <22235437+1715173329@users.noreply.github.com>
2018-01-27 17:52:23 +0800

    Add files via upload

--- v2ray-go.sh
+++ v2ray-go.sh
@@ -20,6 +20,20 @@ function checkenv(){
 		fi
 }
  
+ 
+function checkos(){
+    if [ -f /etc/redhat-release ];then
+        OS='centos'
+    elif [ ! -z "`cat /etc/issue | grep bian`" ];then
+        OS='debian'
+    elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
+        OS='ubuntu'
+    else
+        echo "Not support OS, Please change OS and retry!"
+        exit 1
+    fi
+}
+
 
 function install_v2ray(){
 	rootness
