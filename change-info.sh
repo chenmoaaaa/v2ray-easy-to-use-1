7d23508ea6e04dcfe48d10a102a0c32cb2ddde79
1715173329 <22235437+1715173329@users.noreply.github.com>
2018-01-28 02:13:43 +0800

    Update change-info.sh

--- change-info.sh
+++ change-info.sh
@@ -31,7 +31,7 @@ function change_info(){
     sed -i "s/10000/${PORT}/g" "/etc/v2ray/config.json"
     sed -i "s/3922f464-d02d-4124-82bf-ad350c19aacf/${UUID}/g" "/etc/v2ray/config.json"
     sed -i "s/10000/${PORT}/g" "/usr/local/caddy/Caddyfile"
-    read -p "请输入您的域名：" url
+    stty erase '^H' && read -p "请输入您的域名：" url
     echo ""${url#*"://"}"" > /tmp/caddyaddress.txt
     sed -i "s#/##g" "/tmp/caddyaddress.txt"
     Address=$(cat "/tmp/caddyaddress.txt")
